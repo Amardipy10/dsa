6e4a68481a429cbb1b446f90da1bb10a8674cfe1
Amardipy10 <amardipy10@gmail.com>
2025-07-28 01:01:38 +0530

    selection_sort

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,3 +1,14 @@
+#include<iostream>
+using namespace std;
+
 void selection_sort(int arr[],int n){
-    
+    for(int i=0;i<n-1;i++){
+        int minm=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[minm]){
+                minm=j;
+            }
+        }
+        swap(arr[i],arr[minm]);
+    }
 }
